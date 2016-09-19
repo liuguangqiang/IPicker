e845945e420514794c35527f500b555b90e16c95
Eric <guangqiang.dev@gmail.com>
2016-09-19 09:39:25 +0800

    update sample

--- app/src/main/java/com/liuguangqiang/ipicker/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/ipicker/sample/MainActivity.java
@@ -1,15 +1,22 @@
 package com.liuguangqiang.ipicker.sample;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
-import com.liuguangqiang.ipicker.IPickerActivity;
+import com.liuguangqiang.ipicker.IPicker;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private SelectedAdapter adapter;
+    private ArrayList<String> selectPictures = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +26,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        IPicker.setLimit(5);
         Button button = (Button) findViewById(R.id.open_picker);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, IPickerActivity.class));
+                IPicker.open(getApplicationContext(), selectPictures);
             }
         });
+
+        IPicker.setListener(new IPicker.OnSelectListener() {
+            @Override
+            public void onSelected(List<String> paths) {
+                selectPictures.clear();
+                selectPictures.addAll(paths);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        adapter = new SelectedAdapter(getApplicationContext(), selectPictures);
+        recyclerView = (RecyclerView) findViewById(R.id.rv_photos);
+        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 4, GridLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(adapter);
     }
 
 }
---
+++ app/src/main/java/com/liuguangqiang/ipicker/sample/SelectedAdapter.java
@@ -0,0 +1,49 @@
+package com.liuguangqiang.ipicker.sample;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.liuguangqiang.ipicker.adapters.BaseAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Eric on 16/9/12.
+ */
+public class SelectedAdapter extends BaseAdapter<String, SelectedAdapter.ViewHolder> {
+
+    public SelectedAdapter(Context context, List<String> data) {
+        super(context, data);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(layoutInflater, parent, false);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+        holder.bindData(data.get(position));
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivPhoto;
+
+        public ViewHolder(LayoutInflater layoutInflater, ViewGroup parent, boolean attachToRoot) {
+            super(layoutInflater.inflate(R.layout.item_selected, parent, attachToRoot));
+            ivPhoto = (ImageView) itemView.findViewById(R.id.iv_photo);
+        }
+
+        public void bindData(String path) {
+            Glide.with(itemView.getContext()).load(path).into(ivPhoto);
+        }
+
+    }
+
+}
