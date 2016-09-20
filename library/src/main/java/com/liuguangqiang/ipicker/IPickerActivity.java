45c8a999733fca8a03a7be773a5290d8ebd3c52f
Eric <guangqiang.dev@gmail.com>
2016-09-20 16:27:07 +0800

    Add copyright and remove unused files.

--- library/src/main/java/com/liuguangqiang/ipicker/IPickerActivity.java
+++ library/src/main/java/com/liuguangqiang/ipicker/IPickerActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Eric Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liuguangqiang.ipicker;
 
 import android.Manifest;
@@ -24,14 +40,10 @@ import com.liuguangqiang.ipicker.adapters.BaseAdapter;
 import com.liuguangqiang.ipicker.adapters.PhotosAdapter;
 import com.liuguangqiang.ipicker.entities.Photo;
 import com.liuguangqiang.ipicker.internal.ImageMedia;
-import com.liuguangqiang.ipicker.internal.Logger;
 import com.liuguangqiang.permissionhelper.PermissionHelper;
 import com.liuguangqiang.permissionhelper.annotations.PermissionDenied;
 import com.liuguangqiang.permissionhelper.annotations.PermissionGranted;
 
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
